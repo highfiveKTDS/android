7dc8f10a7522cda05326f164fd01e51317bad341
KTDS_Christof_Kim <taehun3718@gmail.com>
2015-07-28 13:06:15 +0900

    cmooit hello

--- app/src/main/java/com/ktds/android/MainActivity.java
+++ app/src/main/java/com/ktds/android/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void helloWorlds() {
+    public void helloWorldMethod() {
         return;
     }
 
--- app/src/main/java/com/ktds/android/mayPack/File.java
+++ app/src/main/java/com/ktds/android/mayPack/File.java
@@ -6,6 +6,6 @@ package com.ktds.android.mayPack;
 public class File {
     public static void main(String[] args) {
 
-        System.out.println("aaa");
+        System.out.println("hello!");
     }
 }
